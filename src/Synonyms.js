1e96e491c3ea79c9fa6bed661a9aeed93f505754
Sadiyya-k <125921130+Sadiyya-k@users.noreply.github.com>
2023-08-11 13:02:47 +0100

    Changed header background

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -6,7 +6,7 @@ export default function Synonyms(props) {
     return (
       <div className="Synonyms">
         <ul>
-          <strong>Similar: {""}</strong>
+          <strong>Synonyms: {""}</strong>
           <br />
           {props.synonyms.map(function (synonym, index) {
             return <li key={index}>{synonym}</li>;
